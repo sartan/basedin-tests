667701f1fc29eb78db20723733f38d0a5c5c7f91
Alex Sartan <alex@peskydonut.com>
2014-01-09 23:44:57 -0700

    adding more routes

--- phantomcss/tests/temp.js
+++ phantomcss/tests/temp.js
@@ -1,29 +1,52 @@
 var test = require('../includes/bootstrap.js'), phantomcss = test.phantomcss, config = test.config;
 
 
-casper.test.begin('Account and login pages', function suite(test) {
+casper.test.begin('CSS regression tests', function suite(test) {
   casper.start(casper.options.baseURI);
 
   config.resolutions.forEach(function (resolution) {
-    var x = resolution[0], y = resolution[1];
-    test.comment('Testing at ' + x + 'x' + y + ' resolution');
-
+    var x = resolution[0], y = resolution[1], rez_suffix = x + '_' + y;
     casper.viewport(x, y);
 
-    // Login page
-    casper.goto('login');
-    phantomcss.screenshot('body', 'login_' + x + '_' + y);
+    // Home
+    casper.goto('home');
+    phantomcss.screenshot('body', 'home_' + rez_suffix);
 
-    // Customer account page
+    // Login
+    casper.goto('login');
+    phantomcss.screenshot('body', 'login_' + rez_suffix);
+
+    // Customer account
     casper.login(config.customerUser, config.customerPass);
-    phantomcss.screenshot('body', 'customer_account_' + x + '_' + y);
+    phantomcss.screenshot('body', 'customer_account_' + rez_suffix);
     casper.logout();
 
-    // Business account page
+    // Business account
     casper.goto('login');
     casper.login(config.businessUser, config.businessPass);
-    phantomcss.screenshot('body', 'business_account_' + x + '_' + y);
+    phantomcss.screenshot('body', 'business_account_' + rez_suffix);
     casper.logout();
+
+    // Customer registration
+    casper.goto('registerCustomer');
+    phantomcss.screenshot('body', 'register_customer_' + rez_suffix);
+
+    // Business registration
+    casper.goto('registerBusiness');
+    phantomcss.screenshot('body', 'register_business_' + rez_suffix);
+
+    // Plan selection
+    casper.goto('planSelection');
+    phantomcss.screenshot('body', 'plan_selection_' + rez_suffix);
+
+    // Business selection
+    casper.goto('businessSelect');
+    phantomcss.screenshot('body', 'business_select_' + rez_suffix);
+
+    // Category page
+    casper.goto('categoryPage');
+    phantomcss.screenshot('body', 'category_page_' + rez_suffix);
+
   });
 
   casper.then(function() {
